15e0efaeef5c4574742025ca80568e47ef1b4121
haikaljs <haikaljavascript@gmail.com>
2023-10-04 10:40:05 +0800

    Router & pages setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// page components
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/Recipe";
+
+// styles
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipies/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
